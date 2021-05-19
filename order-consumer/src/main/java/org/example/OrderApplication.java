69b83b8d60bf4d5cf40206e94bef1b390c7fd23e
wangpeicai1996 <33630030+wangpeicai1996@users.noreply.github.com>
2021-05-19 20:48:05 +0800

    ribbon负载均衡设置

--- order-consumer/src/main/java/org/example/OrderApplication.java
+++ order-consumer/src/main/java/org/example/OrderApplication.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,6 +11,17 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 public class OrderApplication {
+
+
+    /**
+     * 负载均衡策略设置
+     * 全局设置负载均衡策略，在启动类注入策略bean
+     * @return
+     */
+    @Bean
+    public RandomRule randomRule(){
+        return new RandomRule();
+    }
 
     /**
      * 注入远程调用http工具bean
